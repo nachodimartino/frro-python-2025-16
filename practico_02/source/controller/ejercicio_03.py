ab1e465e7e7a58df35d227a3c1906c787c2ae215
maurojjzz <maurojim123@gmail.com>
2025-06-26 19:09:35 -0300

    ejercicio 3

--- practico_02/source/controller/ejercicio_03.py
+++ practico_02/source/controller/ejercicio_03.py
@@ -11,7 +11,12 @@ comando (estando parados dentro de la carpeta practico_02):
 $PATH$/practico_02> python -m source.controller.ejercicio_03
 """
 
-# Completar
+import main as main
+import source.controller.controller as controller
+import source.util as util
+import source.data.database as database
+import config.test_config as test_config
+import config.ci_cd.deploy_travis as deploy_travis
 
 # NO MODIFICAR - INICIO
 assert main.name == "main"
